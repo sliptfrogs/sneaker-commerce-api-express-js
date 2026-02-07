1a45b917cfaf3d2c11b20976e0d668ca218b4a04
Kimsan <yoeurnkimsan@gmail.com>
2026-02-08 01:43:18 +0700

    fix lint and jest config

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # sneaker-commerce-api-express-js
+
 #Make sure docker is exist in your machine
 #If not install docker first
 #Below how to start Start docker
+
 1. Start Docker Desktop
 2. Open Terminal in project directory
 3. docker compose up -d
-#How to Stop Docker compose
-1. Open terminal here
-2. type: docker compose down
-#start app as dev mode
-1. docker compose logs -f app
+   #How to Stop Docker compose
+4. Open terminal here
+5. type: docker compose down
+   #start app as dev mode
+6. docker compose logs -f app
--- src/config/SequelizeORM.js
+++ src/config/SequelizeORM.js
@@ -3,14 +3,17 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const sequelize = new Sequelize(
-  process.env.DB_NAME,      // database name
-  process.env.DB_USER,      // username
-  process.env.DB_PASSWORD,  // <-- fixed to match .env
+  process.env.DB_NAME, // database name
+  process.env.DB_USER, // username
+  process.env.DB_PASSWORD, // <-- fixed to match .env
   {
     host: process.env.DB_HOST,
     dialect: 'postgres',
     port: process.env.DB_PORT || 5432,
-    dialectOptions: process.env.DB_SSL === 'true' ? { ssl: { rejectUnauthorized: false } } : {},
+    dialectOptions:
+      process.env.DB_SSL === 'true'
+        ? { ssl: { rejectUnauthorized: false } }
+        : {},
     logging: true,
   },
 );
