8eba593178a0d205b14334eec2db4509c778f640
Kimsan <yoeurnkimsan@gmail.com>
2026-02-10 11:32:36 +0700

    Commit CI/CD new pipelines

--- src/config/SequelizeORM.js
+++ src/config/SequelizeORM.js
@@ -1,6 +1,13 @@
 import { Sequelize, DataTypes } from 'sequelize';
 import dotenv from 'dotenv';
-dotenv.config();
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const env = process.env.NODE_ENV || 'development';
+const envFile = env === 'production' ? '.env.production' : '.env.dev';
+
+dotenv.config({ path: path.resolve(__dirname, '../../', envFile) });
 
 export const sequelize = new Sequelize(
   process.env.DB_NAME, // database name
