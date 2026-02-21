1d2dca13b0300834fb0bd5570d51d1ad6df0a1c2
Kimsan <yoeurnkimsan@gmail.com>
2026-02-21 23:02:46 +0700

    Debug: Discount checkout calculation and remove product after checkedout from main

--- src/config/SequelizeORM.js
+++ src/config/SequelizeORM.js
@@ -1,13 +1,5 @@
 import { Sequelize, DataTypes } from 'sequelize';
 import dotenv from 'dotenv';
-import path from 'path';
-// import { fileURLToPath } from 'url';
-
-// const __dirname = path.dirname(fileURLToPath(import.meta.url));
-// const env = process.env.NODE_ENV || 'development';
-// const envFile = env === 'production' ? '.env.production' : '.env.dev';
-
-// dotenv.config({ path: path.resolve(__dirname, '../../', envFile) });
 dotenv.config();
 export const sequelize = new Sequelize(
   process.env.DB_NAME, // database name
